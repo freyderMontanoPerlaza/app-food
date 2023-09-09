8926571e6d9f22f919373c64d1095be0e622156f
freyder montano <143907744+freyderMontanoPerlaza@users.noreply.github.com>
2023-09-09 15:12:01 -0300

    mostrando ventana modal con todad la informacion imagenes,ingredientes, descripcion y mas

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ function iniciarApp() {
     const selectCategorias = document.querySelector('#categorias');
     selectCategorias.addEventListener('change', seleccionarCategoria);
     const resultado = document.querySelector('#resultado');
+    const modal = new bootstrap.Modal('#modal', {});
+
+
 
     obtenerCategorias();
 
@@ -112,8 +115,55 @@ function iniciarApp() {
 
     //funcion seleccionar recetas por el id en base a la solucion del usuario
     function seleccionarRecetaPorId(id) {
-       const url = ` https://themealdb.com/api/json/v1/1/lookup.php?i=${id} `;
+        const url = ` https://themealdb.com/api/json/v1/1/lookup.php?i=${id} `;
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(resultado => mostrarRecetaModal(resultado.meals[0]));
     }
+
+    function mostrarRecetaModal(receta) {
+        console.log(receta);
+
+        const { idMeal, strInstructions, strMeal, strMealThumb, strIngredient } = receta;
+
+        //añadir contenido al modal
+        const modalTiltle = document.querySelector('.modal .modal-title');
+        const modalBody = document.querySelector('.modal .modal-body');
+
+        modalTiltle.textContent = strMeal;
+        modalBody.innerHTML = ` 
+        <img class="img-fluid" src="${strMealThumb}" alt="receta  ${strMeal}" /> 
+        <h3 class="my-3">Instrucciones</h3>  
+        <p>${strInstructions}</p>
+        <h3 class="my-3">Ingredientes y Cantidades</h3>  
+        
+
+        `;
+      
+
+        const listGroup = document.createElement('UL');
+        listGroup.classList.add('list-group');
+        //iterar Ingredientes
+        for (let i = 1; i <= 20; i++) {
+            
+            if(receta[`strIngredient${i}`]){
+                const ingredientes = receta[`strIngredient${i}`];
+                const cantidad = receta[`strIngredient${i}`];
+
+                const ingredienteLi = document.createElement('LI');
+                ingredienteLi.classList.add('list-group-item');
+                ingredienteLi.textContent = `${ingredientes} - ${cantidad}`;
+
+                listGroup.appendChild(ingredienteLi);
+            }
+        }
+
+        modalBody.appendChild(listGroup);
+        //mostrar el modal
+        modal.show();
+
+    }
+
 
     //limpiar los resultado de la consulta
     function limpiarCodigoHtml(selector) {
