4518bb2e720f795fccb23c2b29788f4fcad820f3
freyder montano <143907744+freyderMontanoPerlaza@users.noreply.github.com>
2023-09-09 01:51:21 -0300

    mostrando  las recetas  por pantalla creado codigo con document.createElement y appenChil  y mas por medio de estas clases de bootstrap le agregamos a nuestro s div creado para que las card y el bottom  tenga el estilo por defecto que tre bootstrap

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ function iniciarApp() {
     const selectCategorias = document.querySelector('#categorias');
     selectCategorias.addEventListener('change', seleccionarCategoria);
 
+    //inyectamos el id de resultados
+    const resultado = document.querySelector('#resultado')
+
 
     obtenerCategorias();
 
@@ -32,7 +35,59 @@ function iniciarApp() {
     function seleccionarCategoria(e) {
         const categoria = (e.target.value);
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
-        console.log(url);
+        fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarRecetas(resultado.meals))
+    }
+
+
+    //mostrar recetas
+    function mostrarRecetas(recetas = []){
+        
+        //iterar los resultados
+        recetas.forEach(receta =>{
+            const {idMeal, strMeal, strMealThumb } = receta;
+
+            const recetaContenedor = document.createElement('DIV');
+            recetaContenedor.classList.add('col-md-4');
+
+            const recetaCard = document.createElement('DIV');
+            recetaCard.classList.add('card','mb-4');
+
+
+            //crear la imagen
+            const recetaImagen = document.createElement('IMG');
+            recetaImagen.classList.add('card-img-top');
+            recetaImagen.alt = `Imagen de la recta ${strMeal}`;
+            recetaImagen.src = strMealThumb;
+
+            //crear descripcion
+            const recetaCardBody = document.createElement('DIV');
+            recetaCardBody.classList.add('card-body');
+
+            //crear titulo
+            const recetaHeading = document.createElement('H3');
+            recetaHeading.classList.add('card-title', 'mb-3');
+            recetaHeading.textContent = strMeal;
+
+            //crear el bottom
+            const recetaButton = document.createElement('BUTTON');
+            recetaButton.classList.add('btn', 'btn-danger', 'w-100');
+            recetaButton.textContent = 'Ver Receta';
+
+
+            //ahora pasamos a inyectar en el codigo HTML
+            recetaCardBody.appendChild(recetaHeading);
+            recetaCardBody.appendChild(recetaButton);
+
+            recetaCard.appendChild(recetaImagen);
+            recetaCard.appendChild(recetaCardBody);
+
+            recetaContenedor.appendChild(recetaCard);
+
+            resultado.appendChild(recetaContenedor);
+
+        });
     }
 
 }
